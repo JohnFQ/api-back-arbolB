8f47ecb8b658070e0ea57ce6724db6dddc22e71b
John Fredy Quiceno <john_quiceno23191@elpoli.edu.co>
2022-04-11 19:48:28 -0500

    first commit

---
+++ testDevelop/src/test/java/com/softcaribbean/develop/testDevelop/TestDevelopArbolBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softcaribbean.develop.testDevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDevelopArbolBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
